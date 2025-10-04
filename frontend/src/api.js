2671c452e31ae0218cc0c8bd71cc3689b0dfe395
Aayansh Anand <95929174+aayanshanand@users.noreply.github.com>
2025-10-05 02:22:42 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://helpdeskmini.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000",
 });
 
 export function setToken(token) {
